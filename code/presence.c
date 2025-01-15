2182ef743f63a59eb9e55a10105995e8d942152a
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2025-01-15 12:59:05 +0100

    Dennis part formated and fixed

---
+++ code/presence.c
@@ -0,0 +1,11 @@
+if (presence on one side){
+    if ((presence on opposite-side neighbor node) && (ts < 10)) {
+        Presence Detected;
+        Activate Speaker;
+    } else{
+        Presence Detected on the corresponding side;
+    }
+} else{
+    Deactivate all presence alarms;
+}
+    
